5544b581254c30ead793fcf9dd07d09b984ea2ad
Enes UTKU <ensutku@hotmail.com>
2024-04-29 11:27:18 +0300

    Initial Commit

---
+++ AI Text-To-Image Generation/AI_Text_To_Image_GenerationApp.swift
@@ -0,0 +1,12 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+@main
+struct AI_Text_To_Image_GenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
